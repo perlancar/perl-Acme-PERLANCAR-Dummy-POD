75970a48bb91259693b47315bbf9b82e81f94a04
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-04-14 08:24:22 +0700

    Initial commit

---
+++ lib/Acme/PERLANCAR/Dummy/POD.pm
@@ -0,0 +1,9 @@
+package Acme::PERLANCAR::Dummy::POD;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: Dummy distribution for various POD testing
---
+++ lib/Acme/PERLANCAR/Dummy/POD/OverBack.pm
@@ -0,0 +1,23 @@
+package Acme::PERLANCAR::Dummy::POD::OverBack;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: Testing =over + =back
+
+=head1 DESCRIPTION
+
+A paragraph.
+
+=over
+
+=back
+
+Another paragraph.
+
+A third paragraph.
+
+=cut

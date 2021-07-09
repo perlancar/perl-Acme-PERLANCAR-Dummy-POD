74f5c678dc1d1de35631769ae07802ab10c343d2
perlancar (on pc-home) <perlancar@gmail.com>
2021-07-09 07:38:02 +0700

    LineBreak: add the E<10> + E<8> trick

--- lib/Acme/PERLANCAR/Dummy/POD/LineBreak.pm
+++ lib/Acme/PERLANCAR/Dummy/POD/LineBreak.pm
@@ -10,7 +10,7 @@ package Acme::PERLANCAR::Dummy::POD::LineBreak;
 
 =head1 DESCRIPTION
 
-Notes: EZ<><br> is not recognized.
+Other things that don't work: EZ<><br> is not recognized. EZ<><13> is ignored.
 
 
 =head1 WITHOUT LINEBREAK
@@ -39,6 +39,17 @@ E<lt>I<filename>E<gt> ...
 The result: 1/10. B<pod2text> ignores the linebreak. B<pod2man> creates a blank
 line (new paragraph) instead of line break. B<pod2html> ignores the linebreak.
 
+
+=head1 WITH EZ<><10> + EZ<><8>
+
+B<foobar> [B<--opt1>|B<-O>] [B<--regex>|B<--wildcard>] [B<-a>] [B<-u>] E<10> E<8>[B<--moreoption1>=I<value>] [B<--moreoption2>=I<value>] E<10> E<8>[B<--moreoption3>=I<value>] [B<--moreoption4>=I<value>] E<10> E<8>[B<--moreoption5>=I<value>] [B<--moreoption6>=I<value>] E<10> E<8>[B<--moreoption7>=I<value>] [B<--moreoption8>=I<value>] E<10> E<8>[B<--moreoption9>=I<value>] [B<--moreoption10>=I<value>] E<10> E<8>[B<--moreoption11>=I<value>] [B<--moreoption12>=I<value>] E<10> E<8>E<lt>I<filename>E<gt> ...
+
+The result: 7/10. B<pod2text> renders it as intended. B<pod2man> renders it as
+intended. B<pod2html> ignores the linebreak.
+
+Reference: L<https://www.nntp.perl.org/group/perl.beginners/2006/08/msg85687.html>
+
+
 =head1 WITH =item
 
 =over
@@ -59,8 +70,8 @@ line (new paragraph) instead of line break. B<pod2html> ignores the linebreak.
 
 =item E<lt>I<filename>E<gt> ...
 
-The result: 7/10. B<pod2text> shows it as intended. B<pod2man> shows it as
-intended. B<pod2html> shows a line break and makes all lines bold.
+The result: 7/10. B<pod2text> renders it as intended. B<pod2man> renders it as
+intended. B<pod2html> shows a paragraph break and makes all lines bold.
 
 =back
 
